8837ea8bd7c1210292eecd207d6cec1e4ae44799
Kailas08 <lenvn.ecm@gmail.com>
2024-08-15 09:02:09 +0300

    Add files via upload

--- Module_8_1.py
+++ Module_8_1.py
@@ -6,9 +6,8 @@ def add_everything_up(a, b):
         # Если возникает TypeError, возвращаем строковое представление a и b
         return f"{a}{b}"
 
-# Примеры использования:
-print(add_everything_up(5, 10))        # 15
-print(add_everything_up(3.5, 2.5))     # 6.0
-print(add_everything_up("Hello, ", "World!"))  # Hello, World!
-print(add_everything_up(5, "abc"))     # 5abc
-print(add_everything_up("abc", 5))     # abc5
+print(add_everything_up(5, 10))
+print(add_everything_up(3.5, 2.5))
+print(add_everything_up("Hello, ", "World!"))
+print(add_everything_up(5, "abc"))
+print(add_everything_up("abc", 5))
